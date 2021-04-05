2d03e55e6d08260d14ec259f909a31c6ba86de41
Stephan Wagner <stephanwagner.me@gmail.com>
2021-04-05 16:58:26 +0200

    Update theme backup

--- deploy.js
+++ deploy.js
@@ -4,6 +4,7 @@ const boxen = require('boxen');
 const colors = require('colors/safe');
 const cliProgress = require('cli-progress');
 const Client = require('ftp');
+const path = require('path');
 const fs = require('fs');
 
 // Log an error message
@@ -43,9 +44,10 @@ config.port = config.port || 21;
 config.user = config.user || 'anonymous';
 config.password = config.password || 'secret';
 config.theme = config.theme || 'my-wordpress-theme';
-config.themePath = config.themePath || './';
+config.themeLocal = config.themeLocal || './wp-content/themes';
+config.themeRemote = config.themeRemote || './wp-content/themes';
 config.ignore = config.ignore || [];
-config.backup = config.backup || './wordpress-deploy_backup';
+config.backupFolder = config.backupFolder || './wordpress-deploy_backups';
 
 // Show wordpress-deploy box
 let boxText = colors.brightMagenta('wordpress-deploy') + '\n\n';
@@ -65,9 +67,8 @@ console.log(
 
 // Get all files to upload
 // https://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
-var path = require('path');
 var walk = function (dir, done) {
-  var results = [];
+  var results = [themePathLocal];
   fs.readdir(dir, function (err, list) {
     if (err) {
       return done(err);
@@ -78,7 +79,7 @@ var walk = function (dir, done) {
       if (!file) {
         return done(null, results);
       }
-      file = path.resolve(dir, file);
+      file = dir + '/' + file;
       // Add file if it is not ignored
       if (config.ignore.indexOf(path.basename(file)) === -1) {
         results.push(file);
@@ -109,20 +110,24 @@ const progress = new cliProgress.SingleBar({
   hideCursor: true
 });
 
-// Walk through files and uplaod synchronous
+const themePathLocal = path.normalize(config.themeLocal + '/' + config.theme);
+const themePathRemote = path.normalize(config.themeRemote + '/' + config.theme);
+
+// Abort if theme not found
+if (!fs.existsSync(themePathLocal)) {
+  error('Theme not found', 'Location: ' + themePathLocal);
+  return;
+}
+
+// Walk through files and upload synchronously
 walk(
-  path.normalize(config.themePath + '/' + config.theme),
+  themePathLocal,
 
   // Results listed
   function (err, results) {
     if (err) {
       throw err;
     }
-
-    // Start progress bar
-    progress.start(results.length, 0, {
-      status: 'Preparing to upload files'
-    });
 
     // Init client
     const client = new Client();
@@ -140,55 +145,73 @@ walk(
       const backupDate = dateStr + '_' + timeStr + '_';
 
       // Backup current theme
-      client.rename('test-theme', backupDate + 'test-theme', function (err) {
-        // Callback after upload
-        function uploadCallback(err, progress, client, filelist) {
-          if (err) {
-            throw err;
-          }
-          progress.increment();
+      const backupPath = path.normalize(
+        config.backupFolder + '/' + backupDate + config.theme
+      );
 
-          // End process
-          if (filelist.length === 1) {
-            progress.update({
-              status: 'Upload complete'
-            });
-            progress.stop();
-            client.end();
-          } else {
-            // Upload next file
-            putItems(filelist.slice(1));
-          }
+      client.mkdir(path.normalize(config.backupFolder), true, function (err) {
+        if (err) {
+          throw err;
         }
 
-        // Upload a file or create a directory
-        function putItems(filelist) {
-          let file = filelist[0];
-
-          const fileRemote = file.replace(
-            '/Users/stephanwagner/Sites/wordpress-deploy/',
-            ''
-          );
-
-          // Create a directory
-          if (fs.lstatSync(file).isDirectory()) {
-            progress.update({
-              status: 'Creating directory: ' + path.basename(file)
-            });
-            client.mkdir(fileRemote, true, function (err) {
-              uploadCallback(err, progress, client, filelist);
-            });
-          } else {
-            // Upload a file
-            progress.update({
-              status: 'Uploading file: ' + path.basename(file)
-            });
-            client.put(file, fileRemote, function (err) {
-              uploadCallback(err, progress, client, filelist);
-            });
+        client.rename(themePathRemote, backupPath, function (err) {
+          // Get the remote path
+          function getRemoteFilename(filename) {
+            return filename.replace(themePathLocal, themePathRemote);
           }
-        }
-        putItems(results);
+
+          // Callback after upload
+          function uploadCallback(err, progress, client, filelist) {
+            if (err) {
+              throw err;
+            }
+            progress.increment();
+
+            // End process
+            if (filelist.length === 1) {
+              progress.update({
+                status: 'Upload complete'
+              });
+              progress.stop();
+              client.end();
+            } else {
+              // Upload next file
+              putItems(filelist.slice(1));
+            }
+          }
+
+          // Upload a file or create a directory
+          function putItems(filelist) {
+            const file = filelist[0];
+            const fileRemote = getRemoteFilename(file);
+
+            // Create a directory
+            if (fs.lstatSync(file).isDirectory()) {
+              progress.update({
+                status: 'Creating directory: ' + path.basename(file)
+              });
+              client.mkdir(fileRemote, true, function (err) {
+                uploadCallback(err, progress, client, filelist);
+              });
+            } else {
+              // Upload a file
+              progress.update({
+                status: 'Uploading file: ' + path.basename(file)
+              });
+              client.put(file, fileRemote, function (err) {
+                uploadCallback(err, progress, client, filelist);
+              });
+            }
+          }
+
+          // Start progress bar
+          progress.start(results.length, 0, {
+            status: 'Preparing to upload files'
+          });
+
+          // Start upload
+          putItems(results);
+        });
       });
     });
 
