1d5b2335e0d9d9d9a923945a513ad87f0de9059c
Stephan Wagner <stephanwagner.me@gmail.com>
2021-04-10 13:14:24 +0200

    Fix backup folder name

--- deploy.js
+++ deploy.js
@@ -185,7 +185,7 @@ walk(
       // Create backup date
       const date = new Date();
       const year = date.getFullYear();
-      const month = ('0' + date.getMonth() + 1).slice(-2);
+      const month = ('0' + (date.getMonth() + 1)).slice(-2);
       const day = ('0' + date.getDate()).slice(-2);
       const hour = ('0' + date.getHours()).slice(-2);
       const minutes = ('0' + date.getMinutes()).slice(-2);
